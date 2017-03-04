a486598d8c4cdef989835de29feab989510b1f74
Darre Wong <darrenwongf@gmail.com>
2017-03-04 14:01:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,8 +17,12 @@ EOF
 #Start the IPsec VPN server
 sudo modprobe af_key
 #Create a new Docker container from this image
-sudo docker run \
+docker run \
     --name ipsec-vpn-server \
-    --env-file $PWD/vpn.env \
+    --env-file ./vpn.env \
     --restart=always \
     -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
